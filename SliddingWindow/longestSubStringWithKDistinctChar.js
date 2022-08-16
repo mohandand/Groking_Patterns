372d4896ad1dabcc5d469b14ca2fbf5e109527f6
Mohan Dandigam <mohandand@gmail.com>
2022-08-16 11:47:11 -0700

    Grokking Pattern Coding Practice

--- SliddingWindow/longestSubStringWithKDistinctChar.js
+++ SliddingWindow/longestSubStringWithKDistinctChar.js
@@ -1,21 +1,55 @@
+// function longestSub(str,k){
+//     let map = {};
+//     let startPointer = 0;
+//     let maxLength = 0
+//     for(let i=0 ;i<str.length;i++){
+//         let rChar = str[i];
+//         (map[rChar]) ? map[rChar]++:map[rChar]=1
+//         while(Object.keys(map).length>k){
+//             let lChar = str[startPointer];
+//             map[lChar]--;
+//             if(map[lChar]==0){
+//                 delete map[lChar]
+//             }
+//             startPointer++;
+//         }
+//         maxLength = Math.max(maxLength,i-startPointer+1)
+//     }
+//     return maxLength
+// }
+
+// console.log(longestSub("cbbebi",3))
+
+/* 
+cebbebi 3 distinct charcters
+
+->first loop through str and add it object as key and value
+
+->check keys length is equal or greater given k value then 
+
+remove decrese first key value and if its equal to zero remove it and increst left pointer
+
+*/
+
 function longestSub(str,k){
-    let map = {};
-    let startPointer = 0;
-    let maxLength = 0
-    for(let i=0 ;i<str.length;i++){
-        let rChar = str[i];
-        (map[rChar]) ? map[rChar]++:map[rChar]=1
+    let maxLength = 0;
+    let startPointer=0;
+    let map = {}
+
+    for(let i=0;i<str.length;i++){
+        let rightChar = str[i];
+        (map[rightChar])? map[rightChar]++ : map[rightChar] =1;
+
         while(Object.keys(map).length>k){
-            let lChar = str[startPointer];
-            map[lChar]--;
-            if(map[lChar]==0){
-                delete map[lChar]
+            let leftChar = str[startPointer]
+            map[leftChar]--
+            if(map[leftChar]==0){
+                delete map[leftChar]
             }
             startPointer++;
         }
-        maxLength = Math.max(maxLength,i-startPointer+1)
+        maxLength = Math.max(maxLength ,i-startPointer+1);
     }
-    return maxLength
+ return maxLength
 }
-
 console.log(longestSub("cbbebi",3))
