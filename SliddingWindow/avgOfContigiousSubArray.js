1f8b13151ea3b489ba1785cf691d64c2bec9279f
Mohan Dandigam <mohandand@gmail.com>
2022-08-15 19:38:12 -0700

    Grokking Pattern Coding Practice

--- SliddingWindow/avgOfContigiousSubArray.js
+++ SliddingWindow/avgOfContigiousSubArray.js
@@ -1,6 +1,16 @@
 function avgOfContiguousSubArray(array,k){
-
+    let res = [];
+    let sum = 0.0;
+    let startpointer = 0;
+    for(let i=0;i<array.length;i++){
+        sum +=array[i];
+        if(i>=k-1){
+            res.push(sum/k);
+            sum -=array[startpointer];
+            startpointer++
+        }
+    }  
+    return res   
 }
-
 
 console.log(avgOfContiguousSubArray([1, 3, 2, 6, -1, 4, 1, 8, 2],5))
