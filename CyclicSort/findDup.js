5d7e9a9845e2df57169f55bdb34108ea5b1968f5
STSC <STSC@LENOVO-131105.ad.csueastbay.edu>
2022-08-27 17:05:35 -0700

    Grooking Pattern Practice

--- CyclicSort/findDup.js
+++ CyclicSort/findDup.js
@@ -6,19 +6,37 @@ if no = >check the number and its correct index values are not same then swap ot
 */
 
 function findDup(array){
-    let i = 0;
+    let i=0;
     while(i<array.length){
-        if(array[i] !== i+1){
-            let j=array[i]-1
-            if(array[i]!==array[j]){
-                [array[i],array[j]] =[array[j],array[i]]
-            }else{
-                return array[i]
-            }
+        let j=array[i]-1
+        if(array[i] !== array[j]){
+            [array[i],array[j]] =[array[j],array[i]]
         }else{
             i++
         }
     }
-    return -1
+    for(let i=0;i<array.length;i++){
+        if(array[i] !== i+1){
+            return array[i]
+        }
+    }
 }
 console.log(findDup([1,4,3,2,4]))
+
+// function findDup(array){
+//     let i = 0;
+//     while(i<array.length){
+//         if(array[i] !== i+1){
+//             let j=array[i]-1
+//             if(array[i]!==array[j]){
+//                 [array[i],array[j]] =[array[j],array[i]]
+//             }else{
+//                 return array[i]
+//             }
+//         }else{
+//             i++
+//         }
+//     }
+//     return -1
+// }
+// console.log(findDup([1,4,3,2,4]))
